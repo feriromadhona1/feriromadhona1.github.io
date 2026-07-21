d3ee3a0559df2dcdad1f8616abcf5658882d35f8
Feri Romadhona <feri.romadhona@mncgroup.com>
2026-07-21 16:24:22 +0700

    update form penyedia

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["framer-motion"],
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "",
   },
 };
 
